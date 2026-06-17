df206afac894838126b514c08a6185d7da78fa43
hatemfathy <hatemf934@gmail.com>
2026-06-18 01:24:55 +0300

    create initpolylines in googlemaps

--- lib/widgets/custom_google_maps.dart
+++ lib/widgets/custom_google_maps.dart
@@ -15,12 +15,14 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   late GoogleMapController googleMapController;
   String? mapStyle;
   Set<Marker> myMarker = {};
+  Set<Polyline> polylines = {};
   @override
   void initState() {
     super.initState();
     initpostion();
     initStyleMap();
     intiMarker();
+    initPloyline();
   }
 
   @override
@@ -35,6 +37,7 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
       body: Stack(
         children: [
           GoogleMap(
+            polylines: polylines,
             markers: myMarker,
             style: mapStyle,
             onMapCreated: (controller) {
@@ -99,5 +102,30 @@ class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
       );
     }).toSet();
     myMarker.addAll(markers);
+  }
+
+  void initPloyline() {
+    Polyline polyline1 = Polyline(
+      color: Colors.red,
+      startCap: Cap.roundCap,
+      // patterns: [PatternItem.dot],
+      zIndex: 1,
+      points: [
+        LatLng(29.992870023817286, 31.261876168733952),
+        LatLng(30.02527561784588, 31.211064403414383),
+      ],
+      polylineId: PolylineId("1"),
+    );
+    Polyline polyline2 = Polyline(
+      color: Colors.blue,
+      startCap: Cap.roundCap,
+      points: [
+        LatLng(29.99465409326671, 31.23612696333552),
+        LatLng(30.039839820554775, 31.240246836199272),
+      ],
+      polylineId: PolylineId("2"),
+    );
+    polylines.add(polyline1);
+    polylines.add(polyline2);
   }
 }
